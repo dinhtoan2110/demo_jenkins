d4587bcb62d148d7c5e748b377107181420cd45e
ToanLe <ledinhtoan2110@gmail.com>
2024-12-19 02:08:27 +0700

    first commit

---
+++ a.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // In ra dòng "Hello World"
+    cout << "Hello World!" << endl;
+
+    // Khai báo hai số nguyên
+    double num1, num2;
+
+    // Nhập hai số từ bàn phím
+    cout << "Enter first number: ";
+    cin >> num1;
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    // Thực hiện các phép to
